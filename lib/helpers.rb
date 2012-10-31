9602fc8b2d1441b1ed8af6ce8ee74e0d36da7ae4
Atsushi NAGASE <atsn.ngs@gmail.com>
2012-10-31 23:10:10 +0900

    simple_format helper

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,3 +2,6 @@ include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::GoogleAnalytics
 include Nanoc::Helpers::Youtube
+include Nanoc::Helpers::SimpleFormat
+include Nanoc::Helpers::Text
+include Nanoc::Helpers::HTMLEscape
